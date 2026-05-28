e27092460f40898660b470cb7e1d09544cfd54db
Hypereides <121137355+Hypereides@users.noreply.github.com>
2026-05-28 21:01:18 +0300

    *

--- mobile/getfit/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile/getfit/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import device_info_plus
+import mobile_scanner
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
